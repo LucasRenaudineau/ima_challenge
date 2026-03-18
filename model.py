0b08af1002747e228ef9fa56062c479695f93853
Lucas Renaudineau <lucas.renaudineau@telecom-paris.fr>
2026-03-18 15:36:14 +0100

    cleaning the import of efficientNet

--- model.py
+++ model.py
@@ -10,19 +10,22 @@ from keras.layers import Input
 from load import *
 from data_augmentation import *
 
-base_model = keras.applications.EfficientNetV2S( # looks like a reasonable size for good performances
-    weights='imagenet',
-    input_shape=(368, 368, 3),
-    include_top=False)
-
-inputs = keras.Input(shape=(368, 368, 3))
-x = data_augmentation(inputs)
-scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1) # requires scaled inputs between -1 and 1
-x = scale_layer(x)
-x = base_model(x, training=False)
-x = keras.layers.GlobalAveragePooling2D()(x)
-x = keras.layers.Dropout(0.5)(x)  # Regularize with dropout
-outputs = keras.layers.Dense(13)(x)
-model = keras.Model(inputs, outputs)
-
-model.summary()
+if __name__ == "__main__":
+    base_model = keras.applications.EfficientNetV2S( # looks like a reasonable size for good performances
+        weights='imagenet',
+        input_shape=(368, 368, 3),
+        include_top=False)
+    
+    inputs = keras.Input(shape=(368, 368, 3))
+    x = data_augmentation(inputs)
+    scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1) # requires scaled inputs between -1 and 1
+    x = scale_layer(x)
+    x = base_model(x, training=False)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    x = keras.layers.Dropout(0.5)(x)  # Regularize with dropout
+    outputs = keras.layers.Dense(13)(x)
+    model = keras.Model(inputs, outputs)
+    
+    model.summary()
+    
+    model.save("./outputs/model_not_trained.keras")
