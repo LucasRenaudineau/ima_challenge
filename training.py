b3659e20a538c134dfe9a3984cac63f036285ce5
Lucas Renaudineau <lucas.renaudineau@telecom-paris.fr>
2026-03-23 15:37:57 +0100

    fix printing f1 score

--- training.py
+++ training.py
@@ -70,11 +70,17 @@ if __name__ == "__main__":
             loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             metrics=[
                 keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
-    ]
+            ]
         )
         for epoch in range(10):
             train_one_epoch(model, epoch, True)
+            val_couples = build_predictions(model,validation_ds)
+            score_f1 = compute_f1(val_couples, validation_ds)
+            print(f"The F1 score of the epoch {epoch} is {score_f1}.")
         # Phase 2 of training (with unfrozen base)
         model = keras.models.load_model("./outputs/model_phase1_epoch6.keras") # Take the best model on the validation test !!
         for epoch in range(5):
             train_one_epoch(model,epoch, False)
+            val_couples = build_predictions(model,validation_ds)
+            score_f1 = compute_f1(val_couples, validation_ds)
+            print(f"The F1 score of the epoch {epoch} is {score_f1}.")
