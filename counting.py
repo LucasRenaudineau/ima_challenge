7f73d4ae45c765e36167f1e896092b1bd829beb8
Lucas Renaudineau <lucas.renaudineau@telecom-paris.fr>
2026-04-18 18:41:37 +0200

    counting update

--- counting.py
+++ counting.py
@@ -1,4 +1,13 @@
 from load import *
+
+metadata = pd.read_csv("./IMA205-challenge/train_metadata.csv")
+
+couples = []
+for _, row in metadata.iterrows():
+    img_name = row["ID"] # for instance it can be : "train_00000.png"
+    img_number = int(img_name.split("_")[1].split(".")[0]) # for instance here it would be 0 (the integer)
+    label_index = label_to_index[row["label"]]
+    couples.append((img_number, label_index))
  
 counts = {label: 0 for label in LABELS}
 for _, label_index in couples:
